3fd9425057059f2238b8a74c83b74d5daeef477c
jerrytkp <121157081+jerrytkp@users.noreply.github.com>
2026-09-15 15:08:38 +0100

    remplace test

--- src/components/hero/HeroSlider.tsx
+++ src/components/hero/HeroSlider.tsx
@@ -34,7 +34,7 @@ function HeroSlider() {
           <div className="nolva-hero-content">
             <p className="nolva-hero-tagline text-red-500">TOUT L’ÉVÉNEMENTIEL, UNE SEULE PLATEFORME.</p>
             <h1 className="nolva-hero-title">
-              Votre événement, de l’idée au jour J.<br /><br /><br />
+              Votre événement, de l’idée au jour J.<br /><br />
               <br />
               <span>Prestataires, événements et assistance intelligente réunis sur NOLVA</span>
             </h1>
